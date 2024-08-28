fe9d2e49c4edded26cdfecedefb9b3276c9b6541
ben mccarthy <bmccarthy90@gmail.com>
2024-08-28 17:14:28 -0400

    added some circle OOP code and notes

--- lib/circle.py
+++ lib/circle.py
@@ -4,18 +4,36 @@ class Circle:
     def __init__(self, radius):
         self._radius = None
 
-        self.radius = radius
+        self.radius = radius #this is set using the setter function
 
 
+    # getter function
     @property
     def radius(self):
-        return self.radius
+        # make sure getter refers to _radius
+        return self._radius
     
+    # setter function
     @radius.setter
     def radius(self, new_radius):
-        self._radius = new_radius
+        if (isinstance(new_radius, int) or isinstance(new_radius, float)) and new_radius > 0:
+            self._radius = new_radius
+        else:
+            print(f"radius must be a number greater than zero, not {new_radius}")
 
+    @property
+    def diameter(self):
+        return self.radius * 2
+    
+    @diameter.setter
+    def diameter(self, new_diameter):
+        if (isinstance(new_diameter, int) or isinstance(new_diameter, float)) and new_diameter > 0:
+            self.radius = new_diameter / 2
+        else:
+            print(f"diameter must be a number greater than zero, not {new_diameter}")
     
 c1 = Circle(2)
+
+c1.diameter = 8.5
 
 print(c1.radius)
